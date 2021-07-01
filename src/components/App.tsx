80d4e671f33351bf7c6635b7fd37d9baea6d1418
sonjaolson <olson1sonja@gmail.com>
2021-07-01 13:28:15 -0700

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { hot } from "react-hot-loader";
+import "./../assets/scss/App.scss";
+import { SpotifyAuth, Scopes } from 'react-spotify-auth'
+import 'react-spotify-auth/dist/index.css';
+import { SpotifyApiContext } from 'react-spotify-api';
+import Cookies from 'js-cookie';
+import Player from './Player'
+
+class App extends React.Component<Record<string, unknown>, undefined> {
+  public render() {
+    const token = Cookies.get('spotifyAuthToken')
+    console.log(token)
+    return (
+      <div className="app">
+        <h1>BPM and Key finder</h1>
+        {token ? (
+        <SpotifyApiContext.Provider value={token}>
+          <Player token={token} />
+        </SpotifyApiContext.Provider>
+      ) : (
+        // Display the login page
+        <SpotifyAuth
+          redirectUri='http://localhost:8080/callback'
+          clientID='cb40697df5cf4f64bc29e16354c7d5ef'
+          scopes={[Scopes.userReadPrivate, Scopes.userReadPlaybackPosition, Scopes.userReadPlaybackState, Scopes.streaming, Scopes.userReadCurrentlyPlaying, Scopes.playlistModifyPublic]} // either style will work
+        />
+      )}
+    </div>
+    );
+  }
+}
+
+declare let module: Record<string, unknown>;
+
+export default hot(module)(App);
